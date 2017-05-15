a61776e8d60b6949493d8de6a8dda118b876af3a
Ricky Reusser <rsreusser@gmail.com>
2017-05-15 13:58:59 -0400

    Start adding cli

---
+++ bin/cli.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const spawn = require('child_process').spawn
+const electron = require('electron')
+const path = require('path')
+const serverPath = path.join(__dirname, '../app/main.js')
+
+var args = [ serverPath ]
+  .concat([].concat(process.argv).splice(2))
+  .concat('--not-packaged=true')
+
+var proc = spawn(electron, args, { stdio: 'inherit' })
+proc.on('close', function (code) {
+  process.exit(code)
+})
